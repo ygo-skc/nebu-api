b126d0ef9fb5a49a65dde2fa70bfa3fd8968d8cb
Javi <8519394+rtomyj@users.noreply.github.com>
2026-04-24 23:55:52 -0500

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use tracing_subscriber;
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
+        .with_max_level(tracing::Level::INFO)
         .with_thread_ids(true)
         .json()
         .init();
--- src/server.rs
+++ src/server.rs
@@ -4,11 +4,12 @@ use tokio::net::TcpListener;
 
 pub async fn run() {
     let port = 9030;
-
     tracing::info!("API starting on port {}", port);
+
     let app = Router::new().route("/status", get(status::get));
     let listener = TcpListener::bind(format!("0.0.0.0:{}", port))
         .await
         .unwrap();
+    
     serve(listener, app).await.unwrap();
 }
