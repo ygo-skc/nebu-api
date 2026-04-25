65c256d931995440240e702ba8d1736414b808eb
Javi <8519394+rtomyj@users.noreply.github.com>
2026-04-24 23:43:08 -0500

    Updated port

--- src/server.rs
+++ src/server.rs
@@ -3,8 +3,12 @@ use axum::{Router, routing::get, serve};
 use tokio::net::TcpListener;
 
 pub async fn run() {
-    tracing::info!("API starting on port 8080");
+    let port = 9030;
+
+    tracing::info!("API starting on port {}", port);
     let app = Router::new().route("/status", get(status::get));
-    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", port))
+        .await
+        .unwrap();
     serve(listener, app).await.unwrap();
 }
